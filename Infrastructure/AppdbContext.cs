17be53b479937f2b6eaf390930b1a49fbedb1218
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 18:31:01 +0300

    add init data

--- Infrastructure/AppdbContext.cs
+++ Infrastructure/AppdbContext.cs
@@ -11,6 +11,13 @@ namespace ChessAPI.Infrastructure
             Database.EnsureCreated();
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                new User() { Name = "Firstuser1", Data = new UserData() { Login = "first1", Password = "12345", Role = "User" } },
+                new User() { Name = "Secontuser23", Data = new UserData() { Login = "second2", Password = "12345", Role = "User" } },
+                new User() { Name = "mainAdmin", Data = new UserData() { Login = "admin", Password = "admin", Role = "Admin" } });
 
+        }
     }
 }
