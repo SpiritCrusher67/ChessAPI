f3b997b8c3f503824b1351c8747a530920511748
sanchez.101 <sanchez.101@mail.ru>
2022-01-28 22:56:27 +0300

    add registration model

---
+++ Models/RegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChessAPI.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string Login { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords mismatch")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
