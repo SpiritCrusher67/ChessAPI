fbf3318fcc0fa4896b435d30aba5df2c0dfc338c
sanchez.101 <sanchez.101@mail.ru>
2022-02-01 16:54:14 +0300

    add events listeners

--- Infrastructure/BoardsService.cs
+++ Infrastructure/BoardsService.cs
@@ -1,17 +1,29 @@
-﻿using ChessLibrary.Boards;
+﻿using ChessAPI.Hubs;
+using ChessLibrary.Boards;
+using Microsoft.AspNetCore.SignalR;
 
 namespace ChessAPI.Infrastructure
 {
     public class BoardsService
     {
         protected Dictionary<string,Board> _allActiveRooms = new Dictionary<string,Board>();
+        IHubContext<ChessHub> _hubContext;
 
         public Board this[string roomId] => _allActiveRooms[roomId];
+
+        public BoardsService(IHubContext<ChessHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
 
         public void AddRoom(string roomId, string ownerLogin, Board board)
         {
             _allActiveRooms[roomId] = board;
             NewRoomCreateEvent?.Invoke(roomId, ownerLogin);
+
+            board.CheckHasSeted += async (from, to) => await _hubContext.Clients.Group(roomId).SendAsync("ReceiveMessage", $"SERVER: {from.CurrentSide} side set CHECK to {to.CurrentSide}.");
+            board.CheckMateHasSeted += async (winner) => await _hubContext.Clients.Group(roomId).SendAsync("ReceiveMessage", $"SERVER: {winner.CurrentSide} set CHECK MATE! Match has ended.");
+            board.MoveHasMakedEvent += async (from, to) => await _hubContext.Clients.Group(roomId).SendAsync("ReceiveMessage", $"SERVER: {to.FigureSide.ToString()} side make move from (y:{from.Y},x:{from.X}) to (y:{to.Y},x:{to.X})");
         }
 
         /// <summary>
