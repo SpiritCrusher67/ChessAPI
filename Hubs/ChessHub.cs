732cd8dc8ec6ef4283e5666fecaf0a55f3bd514d
sanchez.101 <sanchez.101@mail.ru>
2022-02-01 16:50:59 +0300

    revrite hub. remove authorize

--- Hubs/ChessHub.cs
+++ Hubs/ChessHub.cs
@@ -2,16 +2,24 @@
 using ChessAPI.Infrastructure;
 using ChessLibrary.Boards;
 using Microsoft.AspNetCore.Authorization;
+using ChessLibrary.Units;
 
 namespace ChessAPI.Hubs
 {
-    [Authorize]
+    //[Authorize]
     public class ChessHub : Hub
     {
         BoardsService _boardsService;
-        public ChessHub(BoardsService boardsService)
+        ILogger<ChessHub> _logger;
+        public ChessHub(BoardsService boardsService, ILogger<ChessHub> logger)
         {
             _boardsService = boardsService;
+            _logger = logger;
+        }
+
+        public async Task SendMessageToRoom(string message, string roomId)
+        {
+            await Clients.Group(roomId).SendAsync("ReceiveMessage", $"{Context.UserIdentifier ?? Context.ConnectionId}: {message}");
         }
 
         public async Task CreateRoom()
@@ -19,41 +27,62 @@ namespace ChessAPI.Hubs
             var board = new Board();
             board.InitializeBoard();
             
-            board.CheckHasSeted += async (from, to) => await Clients.Caller.SendAsync("ReceiveMessage", $"{from.CurrentSide} side set CHECK to {to.CurrentSide}.");
-            board.CheckMateHasSeted += async (winner) => await Clients.Caller.SendAsync("ReceiveMessage", $"{winner.CurrentSide} set CHECK MATE! Match has ended.");
-
             var roomId = Guid.NewGuid().ToString().Substring(0, 6);
-            await Groups.AddToGroupAsync(Context.UserIdentifier!, roomId);
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
 
             _boardsService.AddRoom(roomId, Context.UserIdentifier!, board);
 
-            await Clients.Caller.SendAsync("ReceiveMessage", roomId);
+            await Clients.Caller.SendAsync("ReceiveMessage", $"SERVER: You create new room. Room id: {roomId}");
         }
 
         public async Task JoinRoom(string roomId)
         {
-            var fields = _boardsService[roomId].GetFields();
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
 
-            await Groups.AddToGroupAsync(Context.UserIdentifier!, roomId);
+            await Clients.Group(roomId).SendAsync("ReceiveMessage", $"SERVER: {Context.UserIdentifier ?? Context.ConnectionId} has enterd the room. Match is starting!");
 
-            await Clients.Caller.SendAsync("ReceiveMessage", $"{Context.User!.Identity!.Name} has enterd the room. Match is starting!");
+            await DisplayBoard(roomId);
 
-            await Clients.Group(roomId).SendAsync("UpdateBoard", fields);
-
-            await Clients.GroupExcept(roomId, Context.ConnectionId).SendAsync("MyTurn");
+            await Clients.GroupExcept(roomId, Context.ConnectionId).SendAsync("ChangeTurn");
         }
 
-        public async Task<bool> MakeMove(string roomId, (int, int) from, (int, int) to)
+        public async Task GetAvaliableFields(string roomId, int y, int x)
+        {
+            var fields = _boardsService[roomId].GetAvaliableFields((y, x));
+            if (fields != null)
+                foreach (var field in fields)
+                    await Clients.Caller.SendAsync("SetSelected", field.Y, field.X);
+            
+        }
+
+        private async Task DisplayBoard(string roomId, IEnumerable<Field> fields)
+        {
+            foreach (var field in fields)
+            {
+                await Clients.Group(roomId).SendAsync("UpdateBoard", field.Y, field.X, field.Figure?.Name, (field.IsEmpty) ? "" : field.FigureSide.ToString());
+            }
+        }
+        public async Task DisplayBoard(string roomId) => await DisplayBoard(roomId, _boardsService[roomId].GetFields().Cast<Field>());
+        
+        public async Task MakeMove(string roomId, int yFrom, int xFrom, int yTo, int xTo)
         {
             var board = _boardsService[roomId];
-            if (board.MakeMove(from, to))
+            var fields = board.GetFields();
+            var from = fields[yFrom, xFrom];
+            var to = fields[yTo, xTo];
+            if (board.MakeMove((yFrom,xFrom), (yTo,xTo)))
             {
-                await Clients.Group(roomId).SendAsync("UpdateBoard", board.GetFields());
-                await Clients.GroupExcept(roomId, Context.ConnectionId).SendAsync("MyTurn");
+                await Clients.Group(roomId).SendAsync("UpdateBoard", from.Y, from.X, from.Figure?.Name, (from.IsEmpty) ? "" : from.FigureSide.ToString());
+                await Clients.Group(roomId).SendAsync("UpdateBoard", to.Y, to.X, to.Figure?.Name, (to.IsEmpty) ? "" : to.FigureSide.ToString());
 
-                return true;
+                await Clients.Group(roomId).SendAsync("ChangeTurn");
             }
-            return false;
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            _logger.LogInformation("Someone disconnected wich " + exception?.Message);
+            return base.OnDisconnectedAsync(exception);
         }
     }
 }
